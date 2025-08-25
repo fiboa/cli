08208b04b38845fe8dc58c81f5a4c29c24579bf6
Matthias Mohr <webmaster@mamo-net.de>
2025-08-25 17:43:35 +0200

    Add custom describe command, update to latest vecorel-cli version

--- fiboa_cli/__init__.py
+++ fiboa_cli/__init__.py
@@ -1,17 +1,5 @@
-from .registry import Registry
+from vecorel_cli.registry import Registry
 
-if __name__ == Registry.src_package or __name__ == "__main__":
-    import click
+from .registry import FiboaRegistry
 
-    @click.group()
-    @click.version_option(version=Registry.get_version(), prog_name=Registry.cli_title)
-    def fiboa_cli():
-        pass
-
-    commands = Registry.get_commands()
-    for command in commands:
-        click_cmd = command.get_cli_command(command)
-        click_args = command.get_cli_args().values()
-        for arg in click_args:
-            click_cmd = arg(click_cmd)
-        fiboa_cli.add_command(click_cmd)
+Registry.instance = FiboaRegistry()
--- fiboa_cli/registry.py
+++ fiboa_cli/registry.py
@@ -2,67 +2,28 @@ from vecorel_cli.registry import Registry, VecorelRegistry
 
 
 class FiboaRegistry(VecorelRegistry):
-    """
-    The public package name of the library (e.g. on pypi).
-    """
-
     name: str = "fiboa-cli"
-
-    """
-    The displayable title of the CLI.
-    """
+    project: str = "fiboa"
     cli_title: str = "fiboa CLI"
-
-    """
-    The internal package name of the CLI.
-    This is the name of the folder in which the source files are located.
-    """
     src_package: str = "fiboa_cli"
-
-    # todo: in fiboa CLI add "area", "perimeter", "determination_datetime", "determination_method"
     core_properties = [
         "id",
         "geometry",
         "collection",
+        "metrics:area",
+        "metrics:perimeter",
+        "category",
+        "determination_datetime",
+        "determination_method",
+        "determination_details",
     ]
 
-    # The filenames for datasets (converters) that should be ignored by the CLI.
-    # Always ignores files with a starting "." or "__"
-    ignored_datasets = [
-        "template.py",
-    ]
+    def register_commands(self):
+        super().register_commands()
 
-    def get_commands(self):
-        """
-        The commands that are made available by the CLI.
-        """
-        from vecorel_cli.convert import ConvertData
-        from vecorel_cli.converters import Converters
-        from vecorel_cli.create_geojson import CreateGeoJson
-        from vecorel_cli.create_geoparquet import CreateGeoParquet
-        from vecorel_cli.create_jsonschema import CreateJsonSchema
-        from vecorel_cli.create_stac import CreateStacCollection
-        from vecorel_cli.describe import DescribeFile
-        from vecorel_cli.improve import ImproveData
-        from vecorel_cli.merge import MergeDatasets
-        from vecorel_cli.rename_extension import RenameExtension
-        from vecorel_cli.validate import ValidateData
-        from vecorel_cli.validate_schema import ValidateSchema
+        from .describe import DescribeFiboaFile
 
-        return [
-            ConvertData,
-            Converters,
-            CreateGeoJson,
-            CreateGeoParquet,
-            CreateJsonSchema,
-            CreateStacCollection,
-            DescribeFile,
-            ImproveData,
-            MergeDatasets,
-            RenameExtension,
-            ValidateData,
-            ValidateSchema,
-        ]
+        self.set_command(DescribeFiboaFile)
 
 
 Registry.instance = FiboaRegistry()
