88841f34fba14152eb7dae1d6c9170f7c46066fd
Ivor Bosloper <ivorbosloper@gmail.com>
2025-12-08 00:13:10 +0100

    Update USA converters

--- fiboa_cli/datasets/us_usda_cropland.py
+++ fiboa_cli/datasets/us_usda_cropland.py
@@ -1,18 +1,24 @@
-from os.path import dirname, join
-
 import pandas as pd
 from loguru import logger
 from vecorel_cli.conversion.admin import AdminConverterMixin
 
 from ..conversion.fiboa_converter import FiboaBaseConverter
 from .commons.ec import load_ec_mapping
+from .commons.hcat import AddHCATMixin
 
 
-class Converter(AdminConverterMixin, FiboaBaseConverter):
-    sources = {
-        "https://www.nass.usda.gov/Research_and_Science/Crop-Sequence-Boundaries/datasets/NationalCSB_2016-2023_rev23.zip": [
-            "NationalCSB_2016-2023_rev23/CSB1623.gdb"
-        ]
+class Converter(AdminConverterMixin, AddHCATMixin, FiboaBaseConverter):
+    variants = {
+        "2024": {
+            "https://www.nass.usda.gov/Research_and_Science/Crop-Sequence-Boundaries/datasets/NationalCSB_2017-2024_rev23.zip": [
+                "NationalCSB_2017-2024_rev23/CSB1724.gdb"
+            ]
+        },
+        "2023": {
+            "https://www.nass.usda.gov/Research_and_Science/Crop-Sequence-Boundaries/datasets/NationalCSB_2016-2023_rev23.zip": [
+                "NationalCSB_2016-2023_rev23/CSB1623.gdb"
+            ]
+        },
     }
     id = "us_usda_cropland"
     short_name = "US (USDA CSB)"
@@ -30,19 +36,19 @@ CSB represents non-confidential single crop field boundaries over a set time fra
     columns = {
         "geometry": "geometry",
         "CSBID": "id",
-        "CDL2023": "crop:code",
+        # "CDL2023": "crop:code",
         "crop:name": "crop:name",
         "CNTY": "administrative_area_level_2",
     }
     column_additions = {
         "determination:datetime": "2023-05-01T00:00:00Z",
-        "crop:code_list": "https://fiboa.org/code/us/usda/crop.csv",
     }
     missing_schemas = {
         "properties": {
             "administrative_area_level_2": {"type": "string"},
         }
     }
+    ec_mapping_csv = "https://fiboa.org/code/us/usda/cropland.csv"
 
     def migrate(self, gdf):
         """
@@ -53,18 +59,24 @@ CSB represents non-confidential single crop field boundaries over a set time fra
         geodataframe.Dissolve(method="unary") is **slow** for large datasets
         So we're handling this huge dataset in blocks, states are a natural grouping-method
         """
+        assert self.variant, "Variant must be set"
+        crop_key = f"CDL{self.variant}"
+        self.columns[crop_key] = "crop:code"
+
         gdf = super().migrate(gdf)
         states = list(gdf["STATEFIPS"].unique())
         gdfs = []
         for state in states:
             logger.info(f"Handling State {state}")
             df = gdf[gdf["STATEFIPS"] == state].explode()
-            # TODO is this correct, don't we need `df = df.dissolve?`
-            df.dissolve(by=["CDL2023"], aggfunc="first").explode()
+            df = df.dissolve(by=[crop_key], aggfunc="first", as_index=False).explode()
             gdfs.append(df)
         gdf = pd.concat(gdfs)
         del gdfs
-        mapping = load_ec_mapping(url=join(dirname(__file__), "data-files", "us_usda_cropland.csv"))
-        original_name_mapping = {int(e["original_code"]): e["original_name"] for e in mapping}
-        gdf["crop:name"] = gdf["CDL2023"].map(original_name_mapping)
+        if self.ec_mapping is None:
+            self.ec_mapping = load_ec_mapping(self.ec_mapping_csv, url=self.mapping_file)
+        original_name_mapping = {
+            int(e["original_code"]): e["original_name"] for e in self.ec_mapping
+        }
+        gdf["crop:name"] = gdf[crop_key].map(original_name_mapping)
         return gdf
