2c566227acfcaa221704f8b9bf729a33c7ef6080
Ivor <ivorbosloper@gmail.com>
2026-09-21 23:20:12 +0200

    Use get_urls from SigpacRecintoMixin

--- fiboa_cli/datasets/es_ex.py
+++ fiboa_cli/datasets/es_ex.py
@@ -32,29 +32,3 @@ least 2026-09-10."""
             "admin_municipality_code": {"type": "string"},
         }
     }
-
-    def get_urls(self):
-        from bs4 import BeautifulSoup
-
-        base = "http://sitex.gobex.es/SITEX/centrodescargas/"
-        soup = BeautifulSoup(requests.get(f"{base}viewsubcategoria/45").content, "html.parser")
-        result = {}
-
-        headers = {"X-Requested-With": "XMLHttpRequest", "X-Update": "resultadosdebusqueda"}
-        values = [
-            e.get("value")
-            for e in soup.find("select", id="municipio").find_all("option")
-            if e.get("value")
-        ]
-        for value in values:
-            form = {
-                "_method": "POST",
-                "data[Datos][subcategoria_id]": 45,
-                "data[Datos][nucleospoblacion_id]": value,
-            }
-            response = requests.post(f"{base}listadoresultados", data=form, headers=headers)
-            soup = BeautifulSoup(response.content, "html.parser")
-            matches = soup.find_all("a", href=re.compile(r"/SITEX/centrodescargas/descargar/"))
-            m = matches[1 if self.variant == "2023" else 0].get("href")
-            result[f"http://sitex.gobex.es/{m}"] = ["*.shp"]  # The single shapefile
-        return result
